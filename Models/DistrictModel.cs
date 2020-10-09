7e3f3bb076416f6fac9965960082b45ce8b9d4a6
SzypiTBG <w59276@student.wsiz.rzeszow.pl>
2020-10-09 12:37:35 +0200

    Initial commit

---
+++ Models/DistrictModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTraining.Models
+{
+    public class DistrictModel
+    {
+        public string DistrictItemNumber { get; set; }
+        //@@TODO ogarnąć EventHandler do pobierania zdjęcia z bazy danych Entity Framework Core
+
+        //public string DistrictImage { get; set; }
+        public string DistrictDescription { get; set; }
+        public string DistrictScale { get; set; }
+        public string DistrictLink { get; set; }
+        public string DistrictStreamInformation { get; set; }
+
+
+    }
+}
---
+++ Models/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTraining.Models
+{
+    public class IndexModel
+    {
+        public string IndexTitle { get; set; }
+        //@@TODO ogarnąć EventHandler do pobierania zdjęcia z bazy danych Entity Framework Core
+
+        //public string IndexImage { get; set; }
+        public string IndexDescription { get; set; }
+        public string IndexStreamInformation { get; set; }
+    }
+}
